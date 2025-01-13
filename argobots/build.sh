e41a57e0a08ee443cd27d7b8a704cd4c460da9cf
T S <cactoid@gmail.com>
2025-01-13 14:48:31 +0900

    up

--- argobots/build.sh
+++ argobots/build.sh
@@ -1,3 +1,4 @@
 ./autogen.sh
-./configure --prefix=$PWD/install --enable-affinity --enable-fast=O3 --enable-default-stacksize=1048576
+#./configure --prefix=$PWD/install --enable-affinity --enable-fast=O3 --enable-debug
+./configure --prefix=$PWD/install --enable-affinity --enable-fast=O3
 make && make install
